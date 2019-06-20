51991781d99e05b2d5988910b9610ebbddcf0bd9
Deron Sobers <deronfsobers2@gmail.com>
2019-06-20 08:13:40 -0500

    Updated commments

--- src/Question1Sol.java
+++ src/Question1Sol.java
@@ -11,8 +11,8 @@ public class Question1Sol {
         // for loop iterates through the string passed
         for (int i = 0; i < str.length(); i++) {
 
-            // integer val is assigned to the Octal ASCII
-            // code of the character at index i
+            // integer val is assigned to the ASCII
+            // value of the character at index i
             int val = str.charAt(i);
 
             // if the value at char_set is true, this means that
@@ -20,7 +20,7 @@ public class Question1Sol {
             // ultimately terminating this loop
             if (char_set[val]) return false;
 
-            // val is a integer value equal to the Octal ASCII code of a character
+            // val is a integer value equal to the ASCII value of a character
             // this index is assigned true to confirm the presence of a character
             char_set[val] = true;
         }
@@ -28,17 +28,17 @@ public class Question1Sol {
     }
 
     public static void main(String[] args) {
-        // Character 'c' is Octal ASCII Code 99
-        // Character 'o' is Octal ASCII Code 111
-        // Character 'd' is Octal ASCII Code 100
-        // Character 'e' is Octal ASCII Code 101
-        // Character 'd' is Octal ASCII Code 100
+        // Character 'c' is ASCII value 99
+        // Character 'o' is ASCII value 111
+        // Character 'd' is ASCII value 100
+        // Character 'e' is ASCII value 101
+        // Character 'd' is ASCII value 100
 
         // this string is assigned a value of the word 'coded'
         String a = "coded";
 
         // Because character 'd' is located in this string index 100 which
-        // is the Octal ASCII code for 'd' will be set to equal true.
+        // is the ASCII value for 'd' will be set to equal true.
         // the if statement in isUniqueChars() will check index 100 for a second time
         // and return false because it was previously set to true
 
